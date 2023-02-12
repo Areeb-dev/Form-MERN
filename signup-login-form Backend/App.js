fa00447762a2679f47377a608d99ac3838680c8d
Muhammad Areeb <79369873+Areeb-dev@users.noreply.github.com>
2023-02-12 14:50:33 +0500

    remove double quote

--- signup-login-form Backend/App.js
+++ signup-login-form Backend/App.js
@@ -1,8 +1,8 @@
-const express = require("express")
+const express = require('express')
 const app = express()
 const { mongoose } = require('./config')
-const signupRoute = require("./routes/signUpRoute.js")
-const signInRoute = require("./routes/signInRoutes")
+const signupRoute = require('./routes/signUpRoute.js')
+const signInRoute = require('./routes/signInRoutes')
 
 var db = mongoose.connection
 db.on('error', (err) => {
@@ -13,13 +13,13 @@ db.on('open', async () => {
   console.log('DB running')
 })
 
-let cors = require("cors")
+let cors = require('cors')
 app.use(cors())
 app.use(express.json())
 
-app.use("/", signupRoute)
-app.use("/", signInRoute)
+app.use('/', signupRoute)
+app.use('/', signInRoute)
 
 app.listen(4000, () => {
-  console.log("Server is working")
+  console.log('Server is working')
 })
